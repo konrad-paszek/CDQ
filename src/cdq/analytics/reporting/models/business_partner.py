09e64f3cd0f008c843b2da0413b7097f1e720ed6
konrad-paszek <konradpaszek.dev@gmail.com>
2025-10-26 18:38:06 +0100

    Add VAT number field to BusinessPartnerSchema typemap and project methods

--- src/cdq/analytics/reporting/models/business_partner.py
+++ src/cdq/analytics/reporting/models/business_partner.py
@@ -6,11 +6,12 @@ from cdq.dto import Schema
 @dataclass
 class BusinessPartnerSchema(Schema):
     def typemap_define(self):
-        return {"business_partner_id": str, "name": str, "country": str}
+        return {"business_partner_id": str, "name": str, "country": str, "vat_number": str}
 
     def project(self):
         return {
             self.fields.business_partner_id.name: {"$toString": "$_id"},
             self.fields.name.name: "$companyName",
             self.fields.country.name: "$address.country",
+            self.fields.vat_number.name: "$identifier.vatNumber",
         }
