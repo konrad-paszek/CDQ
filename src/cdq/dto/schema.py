9769d51c1ec7649e1968818c29946e0f98f065b6
konrad-paszek <konradpaszek.dev@gmail.com>
2025-10-26 18:48:37 +0100

    Refactor schema.py: Improve type handling and streamline FieldContainer methods

--- src/cdq/dto/schema.py
+++ src/cdq/dto/schema.py
@@ -1,20 +1,19 @@
-from dataclasses import dataclass
-from typing import Optional, Type
+from dataclasses import dataclass, field
+from typing import Optional, Type, Iterator
 
 import pyarrow as pa
 
 from .base import Dto
 
 
-def _as_arrow(type_):
+def _as_arrow(type_: Type) -> pa.DataType:
     if type_ is str:
         return pa.string()
     if type_ is float:
         return pa.float64()
     if type_ is int:
         return pa.int64()
-    # TODO: any idea how we could map categorical types?
-    return TypeError(f"unsupported type provided {type_}")
+    raise TypeError(f"unsupported type provided {type_}")
 
 
 @dataclass
@@ -23,18 +22,28 @@ class Field:
     type: Type
 
     @property
-    def type_arrow(self):
+    def type_arrow(self) -> pa.DataType:
         return _as_arrow(self.type)
 
-
 class FieldContainer:
-    def __iter__(self):
-        pass
+    def __init__(self):
+        self._fields = {}
+
+    def add(self, field: Field):
+        self._fields[field.name] = field
+
+    def __iter__(self) -> Iterator[Field]:
+        return iter(self._fields.values())
+
+    def __getattr__(self, item):
+        if item in self._fields:
+            return self._fields[item]
+        raise AttributeError(f"'FieldContainer' has no attribute '{item}'")
 
 
 @dataclass
 class Schema(Dto):
-    typemap: Optional[dict] = None
+    typemap: Optional[dict] = field(default=None)
 
     def __post_init__(self):
         self.fields = FieldContainer()
@@ -54,9 +63,5 @@ class Schema(Dto):
         return self.project()
 
     def as_arrow(self) -> pa.Schema:
-        mapping = {}
-        # TODO: why not using field container iterator protocol?
-        for v in self.fields.__dict__.values():
-            if isinstance(v, Field):
-                mapping.update({v.name: v.type_arrow})
+        mapping = {field.name: field.type_arrow for field in self.fields}
         return pa.schema(mapping)
